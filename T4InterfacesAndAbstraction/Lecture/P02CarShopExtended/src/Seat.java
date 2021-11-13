f71081f2cf1d6810080cc64fec4afb25f55832c6
georgi-rm <georgi.rm@gmail.com>
2021-11-13 20:23:29 +0200

    Problem 2 "Car Shop Extended" from Lecture: Interfaces and Abstraction

---
+++ T4InterfacesAndAbstraction/Lecture/P02CarShopExtended/src/Seat.java
@@ -0,0 +1,18 @@
+public class Seat extends CarImpl implements Sellable{
+    private final Double price;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced, Double price) {
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%n%s sells for %.6f", super.toString(), this.getModel(), this.getPrice());
+    }
+}
