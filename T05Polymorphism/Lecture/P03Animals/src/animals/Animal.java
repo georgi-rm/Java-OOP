d16c41cb5abdcf382664905e192532f3f7651ac4
georgi-rm <georgi.rm@gmail.com>
2021-11-16 23:19:00 +0200

    Problem 3 "Animals" from Lecture: Polymorphism

---
+++ T05Polymorphism/Lecture/P03Animals/src/animals/Animal.java
@@ -0,0 +1,23 @@
+package animals;
+
+public abstract class Animal {
+    private final String name;
+    private final String favouriteFood;
+
+    protected Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s", this.getName(), this.getFavouriteFood());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+}
