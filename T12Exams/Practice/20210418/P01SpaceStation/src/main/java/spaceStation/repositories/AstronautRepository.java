aa2e3668ab4c9db4b4fdfc99e7238b64e06d8b45
georgi-rm <georgi.rm@gmail.com>
2021-12-11 22:55:47 +0200

    Problem 1 "Space Station Structure" from Exam Practice on 18 April 2021

---
+++ T12Exams/Practice/20210418/P01SpaceStation/src/main/java/spaceStation/repositories/AstronautRepository.java
@@ -0,0 +1,43 @@
+package spaceStation.repositories;
+
+import spaceStation.models.astronauts.Astronaut;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class AstronautRepository implements Repository<Astronaut> {
+    private Collection<Astronaut> astronauts;
+
+    public AstronautRepository() {
+        this.astronauts = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<Astronaut> getModels() {
+        return Collections.unmodifiableCollection(this.astronauts);
+    }
+
+    @Override
+    public void add(Astronaut model) {
+        this.astronauts.add(model);
+    }
+
+    @Override
+    public boolean remove(Astronaut model) {
+        return this.astronauts.remove(model);
+    }
+
+    @Override
+    public Astronaut findByName(String name) {
+        if (astronauts.isEmpty()) {
+            return null;
+        }
+        for (Astronaut astronaut : astronauts) {
+            if (astronaut.getName().equals(name)) {
+                return astronaut;
+            }
+        }
+        return null;
+    }
+}
